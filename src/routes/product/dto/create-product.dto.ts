fd63fff22ed68985adac394436b57a5d6ecf8b94
Karo8870 <cazacuchristian@gmail.com>
2024-02-28 18:41:11 +0200

    Tested version v01

--- src/routes/product/dto/create-product.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateProductDto {
-	@IsNotEmpty()
-	@IsString()
-	@ApiProperty()
-	ean: string;
-}
