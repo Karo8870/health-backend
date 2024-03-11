cf5796814053c678f336bcd9dc5fcf5c5abf2840
Karo8870 <cazacuchristian@gmail.com>
2024-03-11 22:15:53 +0200

    Added date and modularized code

--- src/routes/post/post.service.ts
+++ src/routes/post/post.service.ts
@@ -14,14 +14,57 @@ import { ClsService } from 'nestjs-cls';
 import { AuthClsStore } from '../auth/auth.guard';
 import { and, count, eq, isNull, sql } from 'drizzle-orm';
 import { ReviewPostDto } from './dto/review-post.dto';
-import { alias } from 'drizzle-orm/pg-core';
+import { alias, PgColumn } from 'drizzle-orm/pg-core';
 
 @Injectable()
 export class PostService {
+	private ownReview = alias(postReviews, 'ownReview');
+	private commentAuthor = alias(users, 'commentAuthor');
+
 	constructor(
 		@Inject('DB') private db: NodePgDatabase<typeof schema>,
 		private cls: ClsService<AuthClsStore>
 	) {}
+
+	generateSelect(id: number | PgColumn) {
+		return this.db
+			.select({
+				title: posts.title,
+				body: postContents.content,
+				date: posts.date,
+				upVotes: count(
+					sql`DISTINCT CASE WHEN ${eq(postReviews.like, true)} THEN 1 END`
+				),
+				downVotes: count(
+					sql`DISTINCT CASE WHEN ${eq(postReviews.like, false)} THEN 1 END`
+				),
+				comments: sql`COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT('id', ${comments.id}, 'body', ${comments.body}, 'date', ${comments.date}, 'author', JSONB_BUILD_OBJECT('id', ${this.commentAuthor.id}, 'user', ${this.commentAuthor.user}, 'firstName', ${this.commentAuthor.firstName}, 'lastName', ${this.commentAuthor.lastName}, 'email', ${this.commentAuthor.email}))) FILTER (WHERE ${comments.id} IS NOT NULL), '[]'::jsonb)`,
+				author: {
+					id: users.id,
+					user: users.user,
+					firstName: users.firstName,
+					lastName: users.lastName,
+					email: users.email
+				},
+				vote: this.ownReview.like
+			})
+			.from(posts)
+			.leftJoin(postContents, eq(postContents.postID, id))
+			.leftJoin(
+				this.ownReview,
+				and(
+					eq(this.ownReview.postID, id),
+					eq(this.ownReview.userID, this.cls.get('userID'))
+				)
+			)
+			.leftJoin(users, eq(users.id, posts.authorID))
+			.leftJoin(postReviews, eq(postReviews.postID, id))
+			.leftJoin(comments, eq(comments.postID, id))
+			.leftJoin(
+				this.commentAuthor,
+				eq(this.commentAuthor.id, comments.authorID)
+			);
+	}
 
 	async create(createPostDto: CreatePostDto) {
 		try {
@@ -62,174 +105,54 @@ export class PostService {
 	}
 
 	async findOne(id: number) {
-		const ownReview = alias(postReviews, 'ownReview');
-		const commentAuthor = alias(users, 'commentAuthor');
-
 		return (
-			await this.db
-				.select({
-					title: posts.title,
-					body: postContents.content,
-					date: posts.date,
-					upVotes: count(
-						sql`DISTINCT CASE WHEN ${eq(postReviews.like, true)} THEN 1 END`
-					),
-					downVotes: count(
-						sql`DISTINCT CASE WHEN ${eq(postReviews.like, false)} THEN 1 END`
-					),
-					comments: sql`COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT('id', ${comments.id}, 'body', ${comments.body}, 'date', ${comments.date}, 'author', JSONB_BUILD_OBJECT('id', ${commentAuthor.id}, 'user', ${commentAuthor.user}, 'firstName', ${commentAuthor.firstName}, 'lastName', ${commentAuthor.lastName}, 'email', ${commentAuthor.email}))) FILTER (WHERE ${comments.id} IS NOT NULL), '[]'::jsonb)`,
-					author: {
-						id: users.id,
-						user: users.user,
-						firstName: users.firstName,
-						lastName: users.lastName,
-						email: users.email
-					},
-					vote: ownReview.like
-				})
-				.from(posts)
-				.leftJoin(postContents, eq(postContents.postID, id))
-				.leftJoin(
-					ownReview,
-					and(
-						eq(ownReview.postID, id),
-						eq(ownReview.userID, this.cls.get('userID'))
-					)
-				)
-				.leftJoin(users, eq(users.id, posts.authorID))
-				.leftJoin(postReviews, eq(postReviews.postID, id))
-				.leftJoin(comments, eq(comments.postID, id))
-				.leftJoin(commentAuthor, eq(commentAuthor.id, comments.authorID))
+			await this.generateSelect(id)
 				.where(eq(posts.id, id))
-				.groupBy(postContents.content, posts.title, users.id, ownReview.like)
+				.groupBy(
+					postContents.content,
+					posts.title,
+					posts.date,
+					users.id,
+					this.ownReview.like
+				)
 				.limit(1)
 		)[0];
 	}
 
 	async findMany(ean: string) {
-		const ownReview = alias(postReviews, 'ownReview');
-		const commentAuthor = alias(users, 'commentAuthor');
-
-		return this.db
-			.select({
-				title: posts.title,
-				body: postContents.content,
-				date: posts.date,
-				upVotes: count(
-					sql`DISTINCT CASE WHEN ${eq(postReviews.like, true)} THEN 1 END`
-				),
-				downVotes: count(
-					sql`DISTINCT CASE WHEN ${eq(postReviews.like, false)} THEN 1 END`
-				),
-				comments: sql`COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT('id', ${comments.id}, 'body', ${comments.body}, 'date', ${comments.date}, 'author', JSONB_BUILD_OBJECT('id', ${commentAuthor.id}, 'user', ${commentAuthor.user}, 'firstName', ${commentAuthor.firstName}, 'lastName', ${commentAuthor.lastName}, 'email', ${commentAuthor.email}))) FILTER (WHERE ${comments.id} IS NOT NULL), '[]'::jsonb)`,
-				author: {
-					id: users.id,
-					user: users.user,
-					firstName: users.firstName,
-					lastName: users.lastName,
-					email: users.email
-				},
-				vote: ownReview.like
-			})
-			.from(posts)
-			.leftJoin(postContents, eq(postContents.postID, posts.id))
-			.leftJoin(
-				ownReview,
-				and(
-					eq(ownReview.postID, posts.id),
-					eq(ownReview.userID, this.cls.get('userID'))
-				)
-			)
-			.leftJoin(users, eq(users.id, posts.authorID))
-			.leftJoin(postReviews, eq(postReviews.postID, posts.id))
-			.leftJoin(comments, eq(comments.postID, posts.id))
-			.leftJoin(commentAuthor, eq(commentAuthor.id, comments.authorID))
+		return this.generateSelect(posts.id)
 			.where(eq(posts.productEAN, ean.replace(/^0/, '')))
-			.groupBy(postContents.content, posts.title, users.id, ownReview.like);
+			.groupBy(
+				postContents.content,
+				posts.title,
+				posts.date,
+				users.id,
+				this.ownReview.like
+			);
 	}
 
 	async findGeneral() {
-		const ownReview = alias(postReviews, 'ownReview');
-		const commentAuthor = alias(users, 'commentAuthor');
-
-		return this.db
-			.select({
-				title: posts.title,
-				body: postContents.content,
-				date: posts.date,
-				upVotes: count(
-					sql`DISTINCT CASE WHEN ${eq(postReviews.like, true)} THEN 1 END`
-				),
-				downVotes: count(
-					sql`DISTINCT CASE WHEN ${eq(postReviews.like, false)} THEN 1 END`
-				),
-				comments: sql`COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT('id', ${comments.id}, 'body', ${comments.body}, 'date', ${comments.date}, 'author', JSONB_BUILD_OBJECT('id', ${commentAuthor.id}, 'user', ${commentAuthor.user}, 'firstName', ${commentAuthor.firstName}, 'lastName', ${commentAuthor.lastName}, 'email', ${commentAuthor.email}))) FILTER (WHERE ${comments.id} IS NOT NULL), '[]'::jsonb)`,
-				author: {
-					id: users.id,
-					user: users.user,
-					firstName: users.firstName,
-					lastName: users.lastName,
-					email: users.email
-				},
-				vote: ownReview.like
-			})
-			.from(posts)
-			.leftJoin(postContents, eq(postContents.postID, posts.id))
-			.leftJoin(
-				ownReview,
-				and(
-					eq(ownReview.postID, posts.id),
-					eq(ownReview.userID, this.cls.get('userID'))
-				)
-			)
-			.leftJoin(users, eq(users.id, posts.authorID))
-			.leftJoin(postReviews, eq(postReviews.postID, posts.id))
-			.leftJoin(comments, eq(comments.postID, posts.id))
-			.leftJoin(commentAuthor, eq(commentAuthor.id, comments.authorID))
+		return this.generateSelect(posts.id)
 			.where(isNull(posts.productEAN))
-			.groupBy(postContents.content, posts.title, users.id, ownReview.like);
+			.groupBy(
+				postContents.content,
+				posts.title,
+				posts.date,
+				users.id,
+				this.ownReview.like
+			);
 	}
 
 	async findOwn() {
-		const ownReview = alias(postReviews, 'ownReview');
-		const commentAuthor = alias(users, 'commentAuthor');
-
-		return this.db
-			.select({
-				body: postContents.content,
-				title: posts.title,
-				date: posts.date,
-				upVotes: count(
-					sql`DISTINCT CASE WHEN ${eq(postReviews.like, true)} THEN 1 END`
-				),
-				downVotes: count(
-					sql`DISTINCT CASE WHEN ${eq(postReviews.like, false)} THEN 1 END`
-				),
-				comments: sql`COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT('id', ${comments.id}, 'body', ${comments.body}, 'date', ${comments.date}, 'author', JSONB_BUILD_OBJECT('id', ${commentAuthor.id}, 'user', ${commentAuthor.user}, 'firstName', ${commentAuthor.firstName}, 'lastName', ${commentAuthor.lastName}, 'email', ${commentAuthor.email}))) FILTER (WHERE ${comments.id} IS NOT NULL), '[]'::jsonb)`,
-				author: {
-					id: users.id,
-					user: users.user,
-					firstName: users.firstName,
-					lastName: users.lastName,
-					email: users.email
-				},
-				vote: ownReview.like
-			})
-			.from(posts)
-			.leftJoin(postContents, eq(postContents.postID, posts.id))
-			.leftJoin(
-				ownReview,
-				and(
-					eq(ownReview.postID, posts.id),
-					eq(ownReview.userID, this.cls.get('userID'))
-				)
-			)
-			.leftJoin(users, eq(users.id, posts.authorID))
-			.leftJoin(postReviews, eq(postReviews.postID, posts.id))
-			.leftJoin(comments, eq(comments.postID, posts.id))
-			.leftJoin(commentAuthor, eq(commentAuthor.id, comments.authorID))
+		return this.generateSelect(posts.id)
 			.where(eq(posts.authorID, this.cls.get('userID')))
-			.groupBy(postContents.content, posts.title, users.id, ownReview.like);
+			.groupBy(
+				postContents.content,
+				posts.title,
+				posts.date,
+				users.id,
+				this.ownReview.like
+			);
 	}
 
 	async update(id: number, updateReviewDto: UpdatePostDto) {
