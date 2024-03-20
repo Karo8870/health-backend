77165668823b0bcc7aeff5a1b765afbc6ff929e8
Karo8870 <cazacuchristian@gmail.com>
2024-03-20 22:03:35 +0200

    Changed AI GET endpoint to POST

--- src/routes/ai/ai.controller.ts
+++ src/routes/ai/ai.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { GeneratePromptDto } from './dto/generate-prompt.dto';
 
 @Controller('ai')
 export class AiController {
-  constructor(private readonly aiService: AiService) {}
+	constructor(private readonly aiService: AiService) {}
 
-  @Get()
-  findMany(@Body() generatePromptDto: GeneratePromptDto) {
-    return this.aiService.generate(generatePromptDto);
-  }
+	@Post()
+	findMany(@Body() generatePromptDto: GeneratePromptDto) {
+		return this.aiService.generate(generatePromptDto);
+	}
 }
