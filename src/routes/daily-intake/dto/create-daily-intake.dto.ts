47db51d01485968cf57c958ae8eae2950ae6280d
Karo8870 <cazacuchristian@gmail.com>
2024-04-06 18:56:28 +0300

    Changed data type

--- src/routes/daily-intake/dto/create-daily-intake.dto.ts
+++ src/routes/daily-intake/dto/create-daily-intake.dto.ts
@@ -1,9 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateDailyIntakeDto {
 	@ApiProperty()
-	@IsString()
 	@IsNotEmpty()
 	data: any;
 }
